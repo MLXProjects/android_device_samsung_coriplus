fd5d637ac292f2f7cbfefcfcc26f853f7db9c4c3
karthick111 <lensflare21@gmail.com>
2016-06-07 20:00:48 +0530

    adapt for commonization
    Change-Id: I6c594fd8e5418a875a3bd48471fb195daba94ae9

--- cm.mk
+++ cm.mk
@@ -15,4 +15,4 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S5312
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=corsicadd TARGET_DEVICE=corsica BUILD_FINGERPRINT=samsung/corsicadd/corsica:4.1.2/JZO54K/S5312DDAMG1:user/release-keys PRIVATE_BUILD_DESC="corsicadd-user 4.1.2 JZO54K S5312DDAMG1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=corsica TARGET_DEVICE=corsica BUILD_FINGERPRINT=samsung/corsica/corsica:4.1.2/JZO54K/S5312DDAMG1:user/release-keys PRIVATE_BUILD_DESC="corsicadd-user 4.1.2 JZO54K S5312DDAMG1 release-keys"
--- device.mk
+++ device.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Call up some makefiles
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/samsung/corsica/corsica-vendor.mk)
@@ -7,14 +23,18 @@ $(call inherit-product, device/samsung/rhea-common/rhea.mk)
 PRODUCT_AAPT_CONFIG := normal ldpi mdpi nodpi
 PRODUCT_AAPT_PREF_CONFIG := ldpi
 
-# Init file
-PRODUCT_COPY_FILES += \
-	device/samsung/corsica/rootdir/init.rhea_ss_corsica.rc:root/init.rhea_ss_corsica.rc \
-        device/samsung/rhea-common/rootdir/fstab.rhea:root/fstab.rhea_ss_corsica.rc
-
-# libinit
+# Init scripts
 PRODUCT_PACKAGES += \
-        libinit_rhea_ss
+    fstab.rhea_ss_corsica \
+    init.rhea_ss_corsica.rc \
+    init.recovery.rhea_ss_corsica.rc \
+    ueventd.rhea_ss_corsica.rc
+
+# display prop for corsica
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=120 \
+    ro.lcd_brightness=160 \
+    ro.lcd_min_brightness=20
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 320
